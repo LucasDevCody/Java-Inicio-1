aa96debf638d1600d766c233efb647b0501d267d
LUCAS DA SILVA <96670121+LucasDevCody@users.noreply.github.com>
2025-03-26 21:37:10 -0400

    pt02commite

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Seja Bem-Vindo, primeiro aceso");
+        System.out.println(" primeira linha de codigo ");
     }
 }
